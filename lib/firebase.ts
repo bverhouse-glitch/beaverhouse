2a93c66858ecd54d1cf9b26d666f8ce7ab46fdd8
박연서 <dustj0217@google.com>
2026-02-11 17:02:45 +0900

    firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,22 @@
 // lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 import { getAuth } from "firebase/auth";
 import { collection, doc, getFirestore, serverTimestamp, setDoc } from 'firebase/firestore';
 
-const firebaseConfig = { 
-    apiKey: "AIzaSyCFJGhBsniHjCazJSo7XJW8IujPmsY9fYg", 
-    authDomain: "bverhouse-13e0e.firebaseapp.com", 
-    projectId: "bverhouse-13e0e", 
-    storageBucket: "bverhouse-13e0e.firebasestorage.app", 
-    messagingSenderId: "537167804501", 
-    appId: "1:537167804501:web:002eded2583f4eff3abcdc", 
-    measurementId: "G-5QXS7ERWJH" 
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ2joS_a2cOh8bNgiNBijS31HrQVe0P4A",
+  authDomain: "beaverhouse-bver.firebaseapp.com",
+  projectId: "beaverhouse-bver",
+  storageBucket: "beaverhouse-bver.firebasestorage.app",
+  messagingSenderId: "69608648615",
+  appId: "1:69608648615:web:799ee2614da314ecb4d082",
+  measurementId: "G-R9KQZ9PW4F"
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
